fcd44ff37f489af1b23d14a1f67d142e50868519
tabITsol <133506587+tabITsol@users.noreply.github.com>
2023-05-15 14:14:08 +0530

    Update AppMenu.js

--- apollo-js-Git/layout/AppMenu.js
+++ apollo-js-Git/layout/AppMenu.js
@@ -2,223 +2,9 @@ import AppSubMenu from './AppSubMenu';
 
 const AppMenu = () => {
     const model = [
+
         {
-            label: 'Dashboards',
-            icon: 'pi pi-home',
-            items: [
-                {
-                    label: 'E-Commerce',
-                    icon: 'pi pi-fw pi-home',
-                    to: '/'
-                },
-                {
-                    label: 'Banking',
-                    icon: 'pi pi-fw pi-image',
-                    to: '/dashboard-banking'
-                }
-            ]
-        },
-        {
-            label: 'Apps',
-            icon: 'pi pi-th-large',
-            items: [
-                {
-                    label: 'Blog',
-                    icon: 'pi pi-fw pi-comment',
-                    items: [
-                        {
-                            label: 'List',
-                            icon: 'pi pi-fw pi-image',
-                            to: '/apps/blog/list'
-                        },
-                        {
-                            label: 'Detail',
-                            icon: 'pi pi-fw pi-list',
-                            to: '/apps/blog/detail'
-                        },
-                        {
-                            label: 'Edit',
-                            icon: 'pi pi-fw pi-pencil',
-                            to: '/apps/blog/edit'
-                        }
-                    ]
-                },
-                {
-                    label: 'Calendar',
-                    icon: 'pi pi-fw pi-calendar',
-                    to: '/apps/calendar'
-                },
-                {
-                    label: 'Chat',
-                    icon: 'pi pi-fw pi-comments',
-                    to: '/apps/chat'
-                },
-                {
-                    label: 'Files',
-                    icon: 'pi pi-fw pi-folder',
-                    to: '/apps/files'
-                },
-                {
-                    label: 'Mail',
-                    icon: 'pi pi-fw pi-envelope',
-                    items: [
-                        {
-                            label: 'Inbox',
-                            icon: 'pi pi-fw pi-inbox',
-                            to: '/apps/mail/inbox'
-                        },
-                        {
-                            label: 'Compose',
-                            icon: 'pi pi-fw pi-pencil',
-                            to: '/apps/mail/compose'
-                        },
-                        {
-                            label: 'Detail',
-                            icon: 'pi pi-fw pi-comment',
-                            to: '/apps/mail/detail/1000'
-                        }
-                    ]
-                },
-                {
-                    label: 'Task List',
-                    icon: 'pi pi-fw pi-check-square',
-                    to: '/apps/tasklist'
-                }
-            ]
-        },
-        {
-            label: 'UI Kit',
-            icon: 'pi pi-fw pi-star-fill',
-            items: [
-                {
-                    label: 'Form Layout',
-                    icon: 'pi pi-fw pi-id-card',
-                    to: '/uikit/formlayout'
-                },
-                {
-                    label: 'Input',
-                    icon: 'pi pi-fw pi-check-square',
-                    to: '/uikit/input'
-                },
-                {
-                    label: 'Float Label',
-                    icon: 'pi pi-fw pi-bookmark',
-                    to: '/uikit/floatlabel'
-                },
-                {
-                    label: 'Invalid State',
-                    icon: 'pi pi-fw pi-exclamation-circle',
-                    to: '/uikit/invalidstate'
-                },
-                {
-                    label: 'Button',
-                    icon: 'pi pi-fw pi-box',
-                    to: '/uikit/button'
-                },
-                {
-                    label: 'Table',
-                    icon: 'pi pi-fw pi-table',
-                    to: '/uikit/table'
-                },
-                {
-                    label: 'List',
-                    icon: 'pi pi-fw pi-list',
-                    to: '/uikit/list'
-                },
-                {
-                    label: 'Tree',
-                    icon: 'pi pi-fw pi-share-alt',
-                    to: '/uikit/tree'
-                },
-                {
-                    label: 'Panel',
-                    icon: 'pi pi-fw pi-tablet',
-                    to: '/uikit/panel'
-                },
-                {
-                    label: 'Overlay',
-                    icon: 'pi pi-fw pi-clone',
-                    to: '/uikit/overlay'
-                },
-                {
-                    label: 'Media',
-                    icon: 'pi pi-fw pi-image',
-                    to: '/uikit/media'
-                },
-                {
-                    label: 'Menu',
-                    icon: 'pi pi-fw pi-bars',
-                    to: '/uikit/menu'
-                },
-                {
-                    label: 'Message',
-                    icon: 'pi pi-fw pi-comment',
-                    to: '/uikit/message'
-                },
-                {
-                    label: 'File',
-                    icon: 'pi pi-fw pi-file',
-                    to: '/uikit/file'
-                },
-                {
-                    label: 'Chart',
-                    icon: 'pi pi-fw pi-chart-bar',
-                    to: '/uikit/charts'
-                },
-                {
-                    label: 'Misc',
-                    icon: 'pi pi-fw pi-circle-off',
-                    to: '/uikit/misc'
-                }
-            ]
-        },
-        {
-            label: 'Prime Blocks',
-            icon: 'pi pi-fw pi-prime',
-            items: [
-                {
-                    label: 'Free Blocks',
-                    icon: 'pi pi-fw pi-eye',
-                    to: '/blocks'
-                },
-                {
-                    label: 'All Blocks',
-                    icon: 'pi pi-fw pi-globe',
-                    url: 'https://blocks.primefaces.org',
-                    target: '_blank'
-                }
-            ]
-        },
-        {
-            label: 'Utilities',
-            icon: 'pi pi-fw pi-compass',
-            items: [
-                {
-                    label: 'PrimeIcons',
-                    icon: 'pi pi-fw pi-prime',
-                    to: '/utilities/icons'
-                },
-                {
-                    label: 'Colors',
-                    icon: 'pi pi-fw pi-palette',
-                    to: '/utilities/colors'
-                },
-                {
-                    label: 'PrimeFlex',
-                    icon: 'pi pi-fw pi-desktop',
-                    url: 'https://www.primefaces.org/primeflex/',
-                    target: '_blank'
-                },
-                {
-                    label: 'Figma',
-                    icon: 'pi pi-fw pi-pencil',
-                    url: 'https://www.figma.com/file/zQOW0XBXdCTqODzEOqwBtt/Preview-%7C-Apollo-2022?node-id=335%3A21768&t=urYI89V3PLNAZEJG-1/',
-                    target: '_blank'
-                }
-            ]
-        },
-        {
-            label: 'Pages',
+            label: 'MAIN PAGES',
             icon: 'pi pi-fw pi-briefcase',
             items: [
                 {
@@ -272,46 +58,18 @@ const AppMenu = () => {
                         }
                     ]
                 },
-                {
-                    label: 'Crud',
-                    icon: 'pi pi-fw pi-pencil',
-                    to: '/pages/crud'
-                },
-                {
-                    label: 'Timeline',
-                    icon: 'pi pi-fw pi-calendar',
-                    to: '/pages/timeline'
-                },
-                {
-                    label: 'Invoice',
-                    icon: 'pi pi-fw pi-dollar',
-                    to: '/pages/invoice'
-                },
+
                 {
                     label: 'About Us',
                     icon: 'pi pi-fw pi-user',
                     to: '/pages/aboutus'
                 },
                 {
-                    label: 'Help',
-                    icon: 'pi pi-fw pi-question-circle',
-                    to: '/pages/help'
-                },
-                {
                     label: 'Not Found',
                     icon: 'pi pi-fw pi-exclamation-circle',
                     to: '/pages/notfound'
                 },
-                {
-                    label: 'Empty',
-                    icon: 'pi pi-fw pi-circle-off',
-                    to: '/pages/empty'
-                },
-                {
-                    label: 'FAQ',
-                    icon: 'pi pi-fw pi-question',
-                    to: '/pages/faq'
-                },
+ 
                 {
                     label: 'Contact Us',
                     icon: 'pi pi-fw pi-phone',
@@ -319,56 +77,92 @@ const AppMenu = () => {
                 }
             ]
         },
+
+
+
         {
-            label: 'E-Commerce',
-            icon: 'pi pi-fw pi-wallet',
+            label: 'Dashboards',
+            icon: 'pi pi-home',
             items: [
                 {
-                    label: 'Product Overview',
+                    label: 'Dashboard',
                     icon: 'pi pi-fw pi-image',
-                    to: '/ecommerce/product-overview'
-                },
-                {
-                    label: 'Product List',
-                    icon: 'pi pi-fw pi-list',
-                    to: '/ecommerce/product-list'
-                },
-                {
-                    label: 'New Product',
-                    icon: 'pi pi-fw pi-plus',
-                    to: '/ecommerce/new-product'
-                },
-                {
-                    label: 'Shopping Cart',
-                    icon: 'pi pi-fw pi-shopping-cart',
-                    to: '/ecommerce/shopping-cart'
-                },
-                {
-                    label: 'Checkout Form',
-                    icon: 'pi pi-fw pi-check-square',
-                    to: '/ecommerce/checkout-form'
-                },
-                {
-                    label: 'Order History',
-                    icon: 'pi pi-fw pi-history',
-                    to: '/ecommerce/order-history'
-                },
-                {
-                    label: 'Order Summary',
-                    icon: 'pi pi-fw pi-file',
-                    to: '/ecommerce/order-summary'
+                    to: '/uikit/charts'
                 }
             ]
         },
+
+        {
+            label: 'MSATER SHEET',
+            icon: 'pi pi-home',
+            items: [
+                {
+                    label: 'FORMS',
+                    icon: 'pi pi-fw pi-home',
+                    to: '/uikit/formlayout'
+                },
+                {
+                    label: 'TABLES',
+                    icon: 'pi pi-fw pi-image',
+                    to: '/uikit/table'
+                }
+            ]
+        },
+
+
+        {
+            label: 'Conustruction',
+            icon: 'pi pi-home',
+            items: [
+                {
+                    label: 'CONTRACT',
+                    icon: 'pi pi-fw pi-home',
+                    to: '/pages/contract'
+                },
+
+                {
+                    label: 'CIVIL',
+                    icon: 'pi pi-fw pi-image',
+                    to: '/pages/civil'
+                },
+
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                },
+            ]
+        },
+
+        {
+            label: 'Apps',
+            icon: 'pi pi-th-large',
+            items: [
+               
+                {
+                    label: 'Calendar',
+                    icon: 'pi pi-fw pi-calendar',
+                    to: '/apps/calendar'
+                },
+                            ]
+        },
+       
         {
             label: 'User Management',
             icon: 'pi pi-fw pi-user',
             items: [
                 {
-                    label: 'List',
+                    label: 'Users',
                     icon: 'pi pi-fw pi-list',
                     to: '/profile/list'
                 },
+
+                {
+                    label: 'Roles',
+                    icon: 'pi pi-fw pi-list',
+                    to: '/profile/list'
+                },
+
                 {
                     label: 'Create',
                     icon: 'pi pi-fw pi-plus',
@@ -376,92 +170,7 @@ const AppMenu = () => {
                 }
             ]
         },
-        {
-            label: 'Hierarchy',
-            icon: 'pi pi-fw pi-align-left',
-            items: [
-                {
-                    label: 'Submenu 1',
-                    icon: 'pi pi-fw pi-align-left',
-                    items: [
-                        {
-                            label: 'Submenu 1.1',
-                            icon: 'pi pi-fw pi-align-left',
-                            items: [
-                                {
-                                    label: 'Submenu 1.1.1',
-                                    icon: 'pi pi-fw pi-align-left'
-                                },
-                                {
-                                    label: 'Submenu 1.1.2',
-                                    icon: 'pi pi-fw pi-align-left'
-                                },
-                                {
-                                    label: 'Submenu 1.1.3',
-                                    icon: 'pi pi-fw pi-align-left'
-                                }
-                            ]
-                        },
-                        {
-                            label: 'Submenu 1.2',
-                            icon: 'pi pi-fw pi-align-left',
-                            items: [
-                                {
-                                    label: 'Submenu 1.2.1',
-                                    icon: 'pi pi-fw pi-align-left'
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    label: 'Submenu 2',
-                    icon: 'pi pi-fw pi-align-left',
-                    items: [
-                        {
-                            label: 'Submenu 2.1',
-                            icon: 'pi pi-fw pi-align-left',
-                            items: [
-                                {
-                                    label: 'Submenu 2.1.1',
-                                    icon: 'pi pi-fw pi-align-left'
-                                },
-                                {
-                                    label: 'Submenu 2.1.2',
-                                    icon: 'pi pi-fw pi-align-left'
-                                }
-                            ]
-                        },
-                        {
-                            label: 'Submenu 2.2',
-                            icon: 'pi pi-fw pi-align-left',
-                            items: [
-                                {
-                                    label: 'Submenu 2.2.1',
-                                    icon: 'pi pi-fw pi-align-left'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Start',
-            icon: 'pi pi-fw pi-download',
-            items: [
-                {
-                    label: 'Buy Now',
-                    icon: 'pi pi-fw pi-shopping-cart',
-                    url: 'https://www.primefaces.org/store'
-                },
-                {
-                    label: 'Documentation',
-                    icon: 'pi pi-fw pi-info-circle',
-                    to: '/documentation'
-                }
-            ]
-        }
+      
     ];
 
     return <AppSubMenu model={model} />;
